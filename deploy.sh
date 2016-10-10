f00612c3672a41d2e3227d3b9f515ebe1e07f85d
rmerrett <reece.merrett@cpit.ac.nz>
2016-10-11 12:18:14 +1300

    Specify registry when logging in to docker remotely

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,16 @@
 set -e
 
 # Circle currently using old version of docker which still uses the now deprecated email option
+echo 'logging in to docker locally'
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
+echo 'tagging image'
 docker tag merrettr/circletest $DOCKER_REPOSITORY
+echo 'pushing image'
 docker push $DOCKER_REPOSITORY
 echo 'copying docker-compose file'
 scp ./docker-compose-prod.yml $DEPLOY_USER@$DEPLOY_HOST:./docker-compose.yml
-echo 'setting docker credentials'
-ssh $DEPLOY_USER@$DEPLOY_HOST "docker login -u  $DOCKER_USER  -p  $DOCKER_PASS"
+echo 'logging in to docker remotely'
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker login -u  $DOCKER_USER  -p  $DOCKER_PASS $DOCKER_REGISTRY"
 echo 'pulling images'
 ssh $DEPLOY_USER@$DEPLOY_HOST "docker pull $DOCKER_REPOSITORY"
 echo 'upping compose'

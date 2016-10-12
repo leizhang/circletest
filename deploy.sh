29bcbb34babd00d9eade2f70f04b57205883805a
rmerrett <reece.merrett@cpit.ac.nz>
2016-10-12 16:16:17 +1300

    Set environment variable in prod dockerfile

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,17 @@ set -e
 echo 'logging in to docker locally'
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
 echo 'tagging image'
-docker tag merrettr/circletest $DOCKER_REPOSITORY
+docker tag merrettr/circletest-app $DOCKER_REGISTRY/merrettr/circletest-app
+docker tag merrettr/circletest-http $DOCKER_REGISTRY/merrettr/circletest-http
 echo 'pushing image'
-docker push $DOCKER_REPOSITORY
+docker push $DOCKER_REGISTRY/merrettr/circletest-app
+docker push $DOCKER_REGISTRY/merrettr/circletest-http
 echo 'copying docker-compose file'
 scp ./docker-compose-prod.yml $DEPLOY_USER@$DEPLOY_HOST:./docker-compose.yml
 echo 'logging in to docker remotely'
 ssh $DEPLOY_USER@$DEPLOY_HOST "docker login -u  $DOCKER_USER  -p  $DOCKER_PASS $DOCKER_REGISTRY"
 echo 'pulling images'
-ssh $DEPLOY_USER@$DEPLOY_HOST "docker pull $DOCKER_REPOSITORY"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker pull $DOCKER_REGISTRY/merrettr/circletest-app"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker pull $DOCKER_REGISTRY/merrettr/circletest-http"
 echo 'upping compose'
 ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose up -d'
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'hello ' . getenv('ENV_VAR');

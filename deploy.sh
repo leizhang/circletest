947d97d75daab0a78017ec62fb3abfe921acaeff
rmerrett <reece.merrett@cpit.ac.nz>
2016-10-12 17:04:29 +1300

    Use .env file

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,31 @@ set -e
 # Circle currently using old version of docker which still uses the now deprecated email option
 echo 'logging in to docker locally'
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
+
 echo 'tagging image'
 docker tag merrettr/circletest-app $DOCKER_REGISTRY/merrettr/circletest-app
 docker tag merrettr/circletest-http $DOCKER_REGISTRY/merrettr/circletest-http
+
 echo 'pushing image'
 docker push $DOCKER_REGISTRY/merrettr/circletest-app
 docker push $DOCKER_REGISTRY/merrettr/circletest-http
+
 echo 'copying docker-compose file'
-scp ./docker-compose-prod.yml $DEPLOY_USER@$DEPLOY_HOST:./docker-compose.yml
+scp ./docker-compose-prod.yml $DEPLOY_USER@$DEPLOY_HOST:./circletest/docker-compose.yml
+echo 'creating env file'
+echo ENV_VAR=$ENV_LINE >> .env
+echo 'copying env file'
+scp ./.env $DEPLOY_USER@$DEPLOY_HOST:./circletest/.env
+
 echo 'logging in to docker remotely'
 ssh $DEPLOY_USER@$DEPLOY_HOST "docker login -u  $DOCKER_USER  -p  $DOCKER_PASS $DOCKER_REGISTRY"
+
 echo 'pulling images'
 ssh $DEPLOY_USER@$DEPLOY_HOST "docker pull $DOCKER_REGISTRY/merrettr/circletest-app"
 ssh $DEPLOY_USER@$DEPLOY_HOST "docker pull $DOCKER_REGISTRY/merrettr/circletest-http"
+
 echo 'upping compose'
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose up -d'
+ssh $DEPLOY_USER@$DEPLOY_HOST './circletest/docker-compose up -d'
+
+echo 'cleaning up files'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'rm -rf ./circletest'
